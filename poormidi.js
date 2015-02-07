cee7b6ea3bdbfd8eb1de80b9adad67fc84b0a21f
D.F.Mac <dfm@DFMac-no-MacBook-Air.local>
2015-02-07 21:15:35 +0900

    1st

---
+++ poormidi.js
@@ -0,0 +1,67 @@
+// poormidi.js (Very Poor) Web MIDI API Wrapper
+// For Google Chrome Only :D
+
+var poormidi = function(){
+  this.midi = null;
+  this.inputs = [];
+  this.outputs = [];
+
+  this.success = function(access){
+    console.log("poormidi.success()");
+    this.midi = access;
+
+    // inputs
+    var it = this.midi.inputs.values();
+    for(var o = it.next(); !o.done; o = it.next()){
+      this.inputs.push(o.value);
+    }
+    console.log("poormidi.success() inputs: "+this.inputs.length);
+
+    for(var cnt=0;cnt<this.inputs.length;cnt++){
+      this.inputs[cnt].onmidimessage = this.onMidiEvent;
+    }
+
+    // outputs
+    var ot = this.midi.outputs.values();
+    for(var o = ot.next(); !o.done; o = ot.next()){
+      this.outputs.push(o.value);
+    }
+    console.log("poormidi.success() outputs: "+this.outputs.length);
+
+  }.bind(this);
+
+  this.failure = function(msg){
+    console.log("poormidi.failure(): "+msg);
+  }.bind(this);
+
+  this.onMidiEvent = function(e){
+    console.log("poormidi.onMidiEvent()");
+  }.bind(this);
+
+  this.setHandler = function(func){
+    console.log("poormidi.setHandler()");
+    this.onMidiEvent = func.bind(this);
+  }.bind(this);
+
+  this.sendNoteOn = function(note,velocity){
+    console.log("poormidi.sendNoteOn()");
+    if(this.outputs.length > 0){
+      for(var cnt=0;cnt<this.outputs.length;cnt++){
+        console.log("poormidi.sendNoteOn() output to :"+this.outputs[cnt].name);
+        this.outputs[cnt].send([0x90,note&0x7f,velocity&0x7f]);
+      }
+    }
+  }.bind(this);
+
+  this.sendNoteOff = function(note){
+    console.log("poormidi.sendNoteOff()");
+    if(this.outputs.length > 0){
+      for(var cnt=0;cnt<this.outputs.length;cnt++){
+        console.log("poormidi.sendNoteOff() output to :"+this.outputs[cnt].name);
+        this.outputs[cnt].send([0x80,note,0]);
+      }
+    }
+  }.bind(this);
+
+  navigator.requestMIDIAccess().then(this.success,this.failure);
+};
---
+++ readme.md
@@ -0,0 +1,47 @@
+# poormidi.js
+
+This is a very simple web midi api wrapper.
+
+## usage
+
+### initialization
+
+```index.html
+<script src="js/poormidi.js"></script>
+```
+
+```js
+var midi = new poormidi();
+```
+
+### receive MIDI message
+
+```js
+midi.setHandler(onMIDIEvent);
+
+function onMIDIEvent(e){
+  var message = e.data[0] & 0xf0;
+  if(message === 0x90){  // Note ON?
+    // 好きな処理
+  }
+}
+```
+
+### send Note On
+
+```js
+// Note No.= 40, Velocity = 100
+midi.sendNoteOn(40,100);
+```
+
+### send Note Off
+
+```js
+// Note No.= 40
+midi.sendNoteOff(40);
+```
+
+That's all!
+Enjoy! :D
+
+
