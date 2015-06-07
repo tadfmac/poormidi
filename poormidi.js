942503a43ec08d14a6a07b2ce23f8b9a12c20b30
D.F.Mac <dfm@DFMac-no-MacBook-Air.local>
2015-06-07 17:33:09 +0900

    P&P feature has now supported.

--- poormidi.js
+++ poormidi.js
@@ -2,37 +2,19 @@
 // For Google Chrome Only :D
 
 // 2015.02.23 Change for P&P on Chrome Canary !!!! (W.I.P. and experimental now)
+// 2015.06.07 P&P feature has now supported.
 
 var poormidi = function(){
   this.midi = null;
   this.inputs = [];
   this.outputs = [];
+  this.timer = null;
 
   this.success = function(access){
     console.log("poormidi.success()");
-
     this.midi = access;
-    this.inputs = [];
-    this.outputs = [];
-
-    // inputs
-    var it = this.midi.inputs.values();
-    for(var o = it.next(); !o.done; o = it.next()){
-      this.inputs.push(o.value);
-    }
-    console.log("poormidi.success() inputs: "+this.inputs.length);
-
-    for(var cnt=0;cnt<this.inputs.length;cnt++){
-      this.inputs[cnt].onmidimessage = this.onMidiEvent;
-    }
-
-    // outputs
-    var ot = this.midi.outputs.values();
-    for(var o = ot.next(); !o.done; o = ot.next()){
-      this.outputs.push(o.value);
-    }
-    console.log("poormidi.success() outputs: "+this.outputs.length);
-
+    this.refreshPorts();
+    this.midi.onstatechange = this.onStateChange;
   }.bind(this);
 
   this.failure = function(msg){
@@ -58,6 +40,17 @@ var poormidi = function(){
     }
   }.bind(this);
 
+  this.onStateChange = function(){
+    console.log("poormidi.onStateChange()");
+    if(this.timer != null){
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(function(){
+      this.refreshPorts();
+      this.timer = null;
+    }.bind(this),300);
+  }.bind(this);
+
   this.sendNoteOff = function(note){
     console.log("poormidi.sendNoteOff()");
     if(this.outputs.length > 0){
@@ -69,7 +62,30 @@ var poormidi = function(){
   }.bind(this);
 
   this.refreshPorts = function(){
-    navigator.requestMIDIAccess().then(this.success,this.failure);
+    console.log("poormidi.refreshPorts()");
+    this.inputs = [];
+    this.outputs = [];
+
+    // inputs
+    var it = this.midi.inputs.values();
+    for(var o = it.next(); !o.done; o = it.next()){
+      this.inputs.push(o.value);
+      console.log("input port: "+o.value.name);
+    }
+    console.log("poormidi.refreshPorts() inputs: "+this.inputs.length);
+
+    for(var cnt=0;cnt<this.inputs.length;cnt++){
+      this.inputs[cnt].onmidimessage = this.onMidiEvent;
+    }
+
+    // outputs
+    var ot = this.midi.outputs.values();
+    for(var o = ot.next(); !o.done; o = ot.next()){
+      this.outputs.push(o.value);
+      console.log("output port: "+o.value.name);
+    }
+    console.log("poormidi.refreshPorts() outputs: "+this.outputs.length);
+
   }.bind(this);
 
   this.onConnect = function(e){
