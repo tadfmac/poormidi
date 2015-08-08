81e190ce6d5c114d20512f54f6d68ff35c5dc7dc
D.F.Mac <dfm@tadfmac1.local>
2015-08-08 11:18:24 +0900

    add midi-channel to args on sendNoteOn(), sendNoteOff(), sendCtlChange()

--- _poormidi.js
+++ _poormidi.js
@@ -4,12 +4,7 @@
 // 2015.02.23 Change for P&P on Chrome Canary !!!! (W.I.P. and experimental now)
 // 2015.06.07 P&P feature has now supported.
 // 2015.08.08 add sendCtlChange()
-// 2015.08.08 On sendNoteOn(), sendNoteOff() and sendCtlChange(), 
-//            arguments are now able to set midi-channel,
-//            and also these can be omitted.
-//            See also comments in this code, for details.
 
-See also comments in this code, for details.
 var poormidi = function(){
   this.midi = null;
   this.inputs = [];
@@ -36,80 +31,32 @@ var poormidi = function(){
     this.onMidiEvent = func.bind(this);
   }.bind(this);
 
-  this.sendNoteOn = function(){
+  this.sendNoteOn = function(note,velocity){
     console.log("poormidi.sendNoteOn()");
-    var note = 0;
-    var velocity = 100;
-    var channel = 0;
-    if(arguments.length == 1){
-      // midi.sendNoteOn(note);
-      note = arguments[0];
-    }else if(arguments.length == 2){
-      // midi.sendNoteOn(note,velocity);
-      note = arguments[0];
-      velocity = arguments[1];
-    }else if(arguments.length == 3){
-      // midi.sendNoteOn(channel,note,velocity);
-      channel = arguments[0] & 0x0f;
-      note = arguments[1];
-      velocity = arguments[2];
-    }else{
-      console.log("poormidi.sendNoteOn:parameter error!!");
-      return;
-    }  
     if(this.outputs.length > 0){
       for(var cnt=0;cnt<this.outputs.length;cnt++){
         console.log("poormidi.sendNoteOn() output to :"+this.outputs[cnt].name);
-        this.outputs[cnt].send([0x90|channel,note&0x7f,velocity&0x7f]);
+        this.outputs[cnt].send([0x90,note&0x7f,velocity&0x7f]);
       }
     }
   }.bind(this);
 
-  this.sendNoteOff = function(){
+  this.sendNoteOff = function(note){
     console.log("poormidi.sendNoteOff()");
-    var note = 0;
-    var channel = 0;
-    if(arguments.length == 1){
-      // midi.sendNoteOff(note);
-      note = arguments[0];
-    }else if(arguments.length == 2){
-      // midi.sendNoteOff(channel,note);
-      channel = arguments[0] & 0x0f;
-      note = arguments[1];
-    }else{
-      console.log("poormidi.sendNoteOff:parameter error!!");
-      return;
-    }  
     if(this.outputs.length > 0){
       for(var cnt=0;cnt<this.outputs.length;cnt++){
         console.log("poormidi.sendNoteOff() output to :"+this.outputs[cnt].name);
-        this.outputs[cnt].send([0x80|channel,note,0]);
+        this.outputs[cnt].send([0x80,note,0]);
       }
     }
   }.bind(this);
 
-  this.sendCtlChange = function(){
+  this.sendCtlChange = function(number,value){
     console.log("poormidi.sendCtlChange()");
-    var channel = 0;
-    var number = 0;
-    var value = 0;
-    if(arguments.length == 2){
-      // midi.sendCtlChange(number,value);
-      number = arguments[0];
-      value = arguments[1];
-    }else if(arguments.length == 3){
-      // midi.sendCtlChange(channel,number,value);
-      channel = arguments[0];
-      number = arguments[1];
-      value = arguments[2];
-    }else{
-      console.log("poormidi.sendCtlChange:parameter error!!");
-      return;
-    }
     if(this.outputs.length > 0){
       for(var cnt=0;cnt<this.outputs.length;cnt++){
         console.log("poormidi.sendCtlChange() output to :"+this.outputs[cnt].name);
-        this.outputs[cnt].send([0xB0|channel,number&0x7f,value&0x7f]);
+        this.outputs[cnt].send([0xB0,number&0x7f,value&0x7f]);
       }
     }
   }.bind(this);
